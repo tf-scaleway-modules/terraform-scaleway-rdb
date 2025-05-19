f22a5b0899569980ba7f573d6b204384a45bd549
omar <omar.trigui@leminnov.com>
2025-05-19 13:56:11 +0200

    wip

--- output.tf
+++ output.tf
@@ -4,10 +4,10 @@ output "pg_conn_str" {
     "postgres://%s:%s@%s:%d/%s?sslmode=disable",
     var.username,
     var.password,
-    scaleway_rdb_instance.main.load_balancer.ip,
-    scaleway_rdb_instance.main.load_balancer.port,
-    scaleway_rdb_instance.main.load_balancer.hostname,
-    scaleway_rdb_instance.main.load_balancer.name,
+    scaleway_rdb_instance.main.load_balancer[0].ip,
+    scaleway_rdb_instance.main.load_balancer[0].port,
+    scaleway_rdb_instance.main.load_balancer[0].hostname,
+    scaleway_rdb_instance.main.load_balancer[0].name,
     var.database_name
   )
   sensitive = true
